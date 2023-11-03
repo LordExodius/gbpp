3191be2e8042ab363f00e515f2351e4a1bb75166
Oscar Yu <oscar45697@gmail.com>
2023-11-03 02:29:48 -0400

    moved doxygen comments to headers

--- CPU.h
+++ CPU.h
@@ -4,15 +4,22 @@ class CPU
 {
 private:
     // Registers
-    Register AF;
-    Register BC;
-    Register DE;
-    Register HL;
-    Register PC;
-    Register SP;
+    Register AF;    ///< `A` and `F` register pair (Accumulator and Flag registers).
+    Register BC;    ///< `B` and `C` register pair  .
+    Register DE;    ///< `D` and `E` register pair.
+    Register HL;    ///< `H` and `L` register pair.
+    Register PC;    ///< Program Counter.
+    Register SP;    ///< Stack Pointer.
 
 public:
+    /**
+     * @brief Constructor for CPU object. Initializes register and timer values appropriately.
+     */
     CPU(/* args */);
+
+    /**
+     * @brief Destroy the CPU::CPU object
+     */
     ~CPU();
 
     // Debug
--- Register.h
+++ Register.h
@@ -3,10 +3,26 @@
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
 
+/**
+ * @brief A register struct. Contains an 8-bit lower and upper register, 
+ * which can also be accessed and written to in 16-bit paired form.
+ */
 struct Register
 {
-    u8 lower;               // F, C, E, L
-    u8 higher;               // A, B, D, H
-    u16 getWord();          // combine lower and upper
-    void setWord(u16);      // split u16
+    u8 lower;           ///< Lower 8-bits of the register pair.
+    u8 higher;          ///< Higher 8-bits of the register pair.
+
+    /**
+     * @brief Get the combined values of a register pair.
+     * 
+     * @return The full 16-bit register pair.
+     */
+    u16 getWord();
+
+    /**
+     * @brief Assigns the value of a register pair.
+     * 
+     * @param word A 16-bit value to assign to a register pair.
+     */
+    void setWord(u16);
 };
