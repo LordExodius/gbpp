d48343e4182dbcef79fe14efa4f278f9e24559be
qynhan <qhan33@uwo.ca>
2023-11-07 20:01:28 -0500

    started on cpu instructions

---
+++ cpuInstructs.h
@@ -0,0 +1,11 @@
+#ifndef CPUINSTRUCTS_H_INCLUDED
+#define CPUINSTRUCTS_H_INCLUDED
+class CPUInstructions
+{
+private:
+
+public:
+    int ExecuteOpcode(BYTE opcode);
+    int ExecuteNextOpcode( );
+}
+#endif
