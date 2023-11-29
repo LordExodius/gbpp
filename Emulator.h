c17a0c84d09df5720c9575a3194334451554c0bd
ameena-zehra <77115945+ameena-zehra@users.noreply.github.com>
2023-11-28 22:57:41 -0500

    Doxygen comments for MMU and Emulator

--- Emulator.h
+++ Emulator.h
@@ -1,3 +1,12 @@
+/**
+ * @class Emulator
+ * @brief Gameboy Emulator
+ *
+ * This is the main emulator class that enables the Gameboy to be run correctly
+ * It simulates the behavior of the target system and provides an interface for interacting
+ * with various components such as the CPU, and Memory Management Unit (MMU).
+ */
+
 #ifndef EMULATOR_H
 #define EMULATOR_H
 
@@ -9,7 +18,7 @@
 #include "Graphics.h"
 
 #define FRAMES_PER_SECOND 60
-#define CYCLES_PER_FRAME CPU_CLOCK_SPEED/FRAMES_PER_SECOND
+#define CYCLES_PER_FRAME CPU_CLOCK_SPEED / FRAMES_PER_SECOND
 
 class Emulator
 {
@@ -17,8 +26,7 @@ private:
     Cartridge cartridge; ///< Cartridge object
     MMU mmu;             ///< MMU object
     CPU cpu;             ///< CPU object
-    Register reg;        ///< Register object
-    Graphics *graphics;
+    Graphics *graphics;  ///< Graphics object
 
 public:
     /**
@@ -30,8 +38,22 @@ public:
      * @brief Destroy the Emulator object
      */
     ~Emulator();
-
+    /**
+     * @brief Main emulation loop for the emulator.
+     * This function simulates the execution of the emulator by continuously processing CPU instructions
+     * for a specified number of cycles, representing a frame in the emulation context.
+     * The loop fetches the next CPU instruction, executes it, and updates the internal timer based on
+     * the number of cycles consumed. The loop continues until the total number of cycles processed
+     * reaches the predefined limit per frame (CYCLES_PER_FRAME).
+     * After completing the required cycles for a frame, the emulator proceeds to render graphics,
+     * providing a visual representation of the current emulation state.
+     */
     void loop();
+    /**
+     * @brief Main execution for the emulator
+     * This function is responsible for running the selected Cartridge and CPU file
+     * simulating the processing of instructions and handling various components.
+     */
     void run();
 };
 
